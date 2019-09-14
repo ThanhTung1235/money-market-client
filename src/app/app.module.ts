30075f2953ffd5c889071dfd2925274efd58a941
ThanhTung1235 <tungltd00602@fpt.edu.vn>
2019-09-14 12:19:15 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './app.route';
+import { CoinListComponent } from './component/coin/coin-list/coin-list.component';
+import { AddCoinComponent } from './component/coin/add-coin/add-coin.component';
+import { MarketListComponent } from './component/market/market-list/market-list.component';
+import { AddMarketComponent } from './component/market/add-market/add-market.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoinListComponent,
+    AddCoinComponent,
+    MarketListComponent,
+    AddMarketComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/component/market/add-market/add-market.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-market',
+  templateUrl: './add-market.component.html',
+  styleUrls: ['./add-market.component.css']
+})
+export class AddMarketComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
